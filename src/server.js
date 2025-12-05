b70976cad849723927ea484756443010bc96d314
Samir Sharif <samirsharif6969@gmail.com>
2025-12-04 23:07:29 -0500

    new commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 import { getEnvVar } from './helper/getEnvVar.js';
 import { ENV_VARS } from './constants/envVars.js';
 
-import { connectToMongoDB } from './db/connectToMongoDb.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { setupLogger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandlerMiddleware } from './middleware/errorHandler.js';
@@ -37,7 +37,7 @@ const PORT = getEnvVar(ENV_VARS.PORT, 3000);
 
 const startServer = async () => {
   try {
-    await connectToMongoDB();
+    await connectMongoDB();
     app.listen(PORT, () => {
       console.log(` Server is running on port ${PORT}`);
     });
