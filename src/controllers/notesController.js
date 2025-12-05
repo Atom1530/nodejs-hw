89355d50e43839c9b51c16faf1e5cbbd4c844fd6
Samir Sharif <samirsharif6969@gmail.com>
2025-12-04 22:56:56 -0500

    new commit

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,7 +1,7 @@
 // controllers/notesController.js';
 
 import createHttpError from 'http-errors';
-import { Notes } from '../model/notes.js';
+import { Notes } from '../models/notes.js';
 
 export const getAllNotes = async (req, res, next) => {
   try {
